5b5c47fe701a032491b26f01b7f4e3e39beaf609
wwts <im@wwts.me>
2021-04-07 23:22:21 +0800

    update

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
-import sys
+from PIL import Image
+import threading
+import time
 import os
+import numpy as np 
+import io
+import datetime
+import subprocess
+import json
+import sys
 import random
-from pydub.utils import mediainfo
+from queue import Queue
 
 #------ define
-#vcodec = 'h264_videotoolbox'
-vcodec = 'libx264'
+vcodec = 'h264_videotoolbox'
+#vcodec = 'h264_qsv'
 #acodec = 'copy'
 acodec = 'aac'
-
-args = '-preset veryfast'
-
-width = 1920
-height = 1080
-
 bv = '3000k'
-ba = '320k'
+ba = '640k'
+resolution = (1280, 720)
+aw_string = 'f32le'
+aw = 32
+ar = 96 * 1000
+ac = 2
+fps = 60
 
 video_media = [
     '.flv',
-    '.mp4'
+    '.mp4',
+	'.webm'
 ]
 
 audio_media = [
@@ -33,6 +42,35 @@ images_media = [
     '.jpeg',
     '.png'
 ]
+
+#----- end defile
+
+video_send_pipe_path = 'video_send.pipe'
+audio_send_pipe_path = 'audio_send.pipe'
+
+video_read_pipe_path = 'video_read.pipe'
+audio_read_pipe_path = 'audio_read.pipe'
+
+video_queue=Queue()
+audio_queue=Queue()
+currentVideo = np.array(Image.new('RGB', resolution, 'black'))
+new_start = False
+
+if os.path.exists(video_send_pipe_path):
+    os.remove(video_send_pipe_path)
+os.mkfifo(video_send_pipe_path)
+
+if os.path.exists(audio_send_pipe_path):
+    os.remove(audio_send_pipe_path)
+os.mkfifo(audio_send_pipe_path)
+
+if os.path.exists(video_read_pipe_path):
+    os.remove(video_read_pipe_path)
+os.mkfifo(video_read_pipe_path)
+
+if os.path.exists(audio_read_pipe_path):
+    os.remove(audio_read_pipe_path)
+os.mkfifo(audio_read_pipe_path)
 
 #------ get work root path
 root_dir= os.path.dirname(sys.argv[0])
@@ -58,48 +96,155 @@ def get_a_media():
     index = random.randint(0,len(media)-1)
     media_file = media[index]
     return os.path.join(media_path,media_file)
-
-def ass_list():
-    ass_path = os.path.join(root_dir,"ass")
-    asss = os.listdir(ass_path)
-    ret = ''
-    for ass in asss:
-        if os.path.splitext(ass)[1] == '.ass':
-            ret = ret + "ass=\'{}\',".format(os.path.join(ass_path,ass))
-    if len(ret) == 0:
-        return None
-    else:
-        return ret[0:-1]
+    
 #------
+
+output_command = [
+    'ffmpeg',
+    #'-loglevel', 'quiet',
+    '-y', '-an', '-re',
+    '-f', 'rawvideo',
+    '-c:v','rawvideo',
+    '-pix_fmt', 'rgb24',
+    '-s', str(resolution[0]) + 'x' + str(resolution[1]),
+    '-r', str(fps),
+    '-i', video_send_pipe_path,
+    '-f', aw_string,
+    '-ar', str(ar),
+    '-ac', str(ac),
+    '-i', audio_send_pipe_path,
+    '-c:v', vcodec,
+    '-b:v', bv,
+    '-c:a', acodec,
+	'-b:a', ba,
+    '-f', 'flv',
+    rtmp_url
+]
+subprocess.Popen(output_command, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+video_output_pipe = os.open(video_send_pipe_path, os.O_WRONLY)
+def video_send_loop():
+    global currentVideo
+    while True:
+        if not video_queue.empty():
+            currentVideo = video_queue.get()
+        os.write(video_output_pipe, currentVideo.tobytes())
+
+video_send_thread = threading.Thread(target=video_send_loop,args=())
+video_send_thread.start()
+
+audio_output_pipe = os.open(audio_send_pipe_path, os.O_WRONLY)
+def audio_send_loop():
+    global new_start
+    while True:
+        if new_start:
+            if audio_queue.qsize() < 20:
+                os.write(audio_output_pipe, bytes([1 for i in range(0,1000)]))
+            else:
+                new_start = False
+        # elif audio_queue.qsize() > 10:
+        #     for i in range(audio_queue.qsize() - 5):
+        #         audio_queue.get()
+        elif not audio_queue.empty():
+            data = audio_queue.get()
+            os.write(audio_output_pipe, data)
+            if len(data) != int(ar/10):
+                os.write(audio_output_pipe, bytes([1 for i in range(0,int(ar/10) - len(data))]))
+        else:
+            os.write(audio_output_pipe, bytes([1 for i in range(0,int(ar/10))]))
+audio_send_thread = threading.Thread(target=audio_send_loop,args=())
+audio_send_thread.start()
+
+def read_video(file):
+    global new_start
+    read_command = [
+        'ffmpeg',
+        '-loglevel', 'quiet',
+        '-re', '-y',
+        '-i', file,
+        '-r', str(fps),
+        '-pixel_format', 'rgb24',
+        '-s', str(resolution[0]) + 'x' + str(resolution[1]),
+        '-f', 'rawvideo',
+        video_read_pipe_path,
+        '-f', aw_string,
+        '-ar', str(ar),
+        '-ac', str(ac),
+        audio_read_pipe_path,
+    ]
+    subprocess.Popen(read_command, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    def video_read_loop():
+        global offset
+        video_input_pipe = open(video_read_pipe_path, 'rb')
+        while True:
+            read = video_input_pipe.read(resolution[0] * resolution[1] * 3)
+            if len(read) == 0:
+                video_input_pipe.close()
+                break
+            im = Image.frombuffer('L', resolution, read)
+            im = im.convert('RGB')
+            video_queue.put(np.array(im))
+    video_read_thread = threading.Thread(target=video_read_loop, args=())
+    video_read_thread.start()
+
+    
+    def audio_read_loop():
+        audio_input_pipe = open(audio_read_pipe_path, 'rb')
+        while True:
+            read = audio_input_pipe.read(int(ar/10))
+            if len(read) == 0:
+                audio_input_pipe.close()
+                break
+            audio_queue.put(read)
+    audio_read_thread = threading.Thread(target=audio_read_loop, args=())
+    new_start = True
+    audio_queue.queue.clear()
+    audio_read_thread.start()
+    audio_read_thread.join()
+
+def read_audio(file):
+    global new_start
+    read_command = ['ffmpeg',
+        '-loglevel', 'quiet',
+        '-re', '-y',
+        '-i', file,
+        '-f', aw_string,
+        '-ar', str(ar),
+        '-ac', str(ac),
+        audio_read_pipe_path,
+        ]
+    subprocess.Popen(read_command, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    def audio_read_loop():
+        audio_input_pipe = open(audio_read_pipe_path, 'rb')
+        while True:
+            read = audio_input_pipe.read(10000)
+            if len(read) == 0:
+                audio_input_pipe.close()
+                break
+            audio_queue.put(read)
+    audio_read_thread = threading.Thread(target=audio_read_loop, args=())
+    new_start = True
+    audio_queue.queue.clear()
+    audio_read_thread.start()
+    audio_read_thread.join()
+
 while True:
     try:
-        command = 'ffmpeg '
         media_file = get_a_media()
-        ass_str = ass_list()
+        print(media_file)
         if os.path.splitext(media_file)[1].lower() in video_media:
-            command = command + "-re -i \"{}\" ".format(media_file)
-            command = command + "-filter_complex \"[0:v]scale={w}:{h}".format(w = width,h = height)
-            if ass_str != None:
-                command = command + "[v1];[v1]{}".format(ass_str)
-            command = command + "\" -vcodec {vcodec} -acodec {acodec} -b:v {bv} -b:a {ba} {args} -f flv \"{output}\"".format(vcodec = vcodec, acodec = acodec, bv = bv, ba = ba, args = args, output = rtmp_url)
-            #print(command)
+            read_video(media_file)
         elif os.path.splitext(media_file)[1].lower() in audio_media:
             img_file = get_a_images()
-            if not os.path.splitext(img_file)[1].lower() in images_media:
-                continue
-            audio = mediainfo(media_file)
-            to = audio['duration']
-            command = command + "-re -i \"{}\" ".format(media_file)
-            command = command + "-f lavfi -i color=c=0x000000:s={w}x{h}:r=30 ".format(w = width, h = height)
-            command = command + "-i \"{}\" ".format(img_file)
-            command = command + "-filter_complex \"[2:v]scale={w}:{h}[v1];[1:v][v1]overlay=0:0".format(w = width, h = height)
-            if ass_str != None:
-                command = command + "[v2];[v2]{}".format(ass_str)
-            command = command + "\" -ss 0 -to {to} -vcodec {vcodec} -acodec {acodec} -b:v {bv} -b:a {ba} {args} -f flv \"{output}\"".format(to = to, vcodec = vcodec, acodec = acodec, bv = bv, ba = ba, args = args, output = rtmp_url)
+            while not os.path.splitext(img_file)[1].lower() in images_media:
+                img_file = get_a_images()
+            im = Image.open(img_file)
+            im = im.resize(resolution)
+            im = im.convert('RGB')
+            currentVideo = np.array(im)
+            read_audio(media_file)
         else:
             continue
-        ret = os.system(command)
-        if ret == 65280:
-            exit(0)
-    except Exception:
+    except Exception as e:
+        print(e)
         continue
